5b352625df935aaf9f5a737c71871d53dbe6b509
Wojciech Pituła <wojciech@swissborg.com>
2025-03-17 12:08:24 +0100

    Detach diagnostics construction and printing

---
+++ decisions4s-core/src/main/scala/decisions4s/internal/DiagnosticsData.scala
@@ -0,0 +1,54 @@
+package decisions4s.internal
+
+import decisions4s.HitPolicy
+
+case class DiagnosticsData(
+    table: DiagnosticsData.Table,
+    input: DiagnosticsData.Input,
+    output: DiagnosticsData.Output,
+)
+
+object DiagnosticsData {
+
+  opaque type RuleIdx <: Int = Int
+  object RuleIdx        {
+    def apply(value: Int): RuleIdx = value
+  }
+  opaque type InputFieldIdx <: Int = Int
+  object InputFieldIdx  {
+    def apply(value: Int): InputFieldIdx = value
+  }
+  opaque type OutputFieldIdx <: Int = Int
+  object OutputFieldIdx {
+    def apply(value: Int): OutputFieldIdx = value
+  }
+
+  case class Table(
+      name: String,
+      hitPolicy: HitPolicy,
+      rules: Seq[Rule],
+  )
+  case class Rule(
+      idx: RuleIdx,
+      annotation: Option[String],
+      renderedConditions: Map[InputFieldIdx, String],
+      evaluationResults: Map[InputFieldIdx, Boolean],
+      output: Option[Rule.Output],
+  )
+  case object Rule      {
+    case class Output(
+        rawValue: Any,
+        fieldValues: Map[OutputFieldIdx, Any],
+    )
+  }
+  case class Input(
+      fieldNames: Map[InputFieldIdx, String],
+      fieldValues: Map[InputFieldIdx, Option[Any]],
+      rawValue: Any,
+  )
+  case class Output(
+      fieldNames: Map[OutputFieldIdx, String],
+      rawValue: Any,
+  )
+
+}
---
+++ decisions4s-core/src/main/scala/decisions4s/internal/DiagnosticsDataBuilder.scala
@@ -0,0 +1,59 @@
+package decisions4s.internal
+
+import decisions4s.*
+import decisions4s.internal.DiagnosticsData.{InputFieldIdx, OutputFieldIdx, RuleIdx}
+import shapeless3.deriving.Const
+
+class DiagnosticsDataBuilder[Input[_[_]], Output[_[_]], Out](r: EvalResult[Input, Output, Out]) {
+  import r.table.given
+  import r.{input, results, table}
+
+  def get: DiagnosticsData = {
+    DiagnosticsData(
+      table = DiagnosticsData.Table(
+        name = r.table.name,
+        hitPolicy = r.table.hitPolicy,
+        rules = r.results.zipWithIndex.map(buildRule.tupled),
+      ),
+      input = DiagnosticsData.Input(
+        fieldNames = summon[HKD[Input]].fieldNames.toIndexedMap(InputFieldIdx.apply),
+        fieldValues = HKDUtils
+          .collectFields(input.mapK[Const[Option[Any]]]([t] => (value: Option[t]) => (value: Option[Any])))
+          .toIndexedMap(InputFieldIdx.apply),
+        rawValue = r.input,
+      ),
+      output = DiagnosticsData.Output(
+        fieldNames = summon[HKD[Output]].fieldNames.toIndexedMap(OutputFieldIdx.apply),
+        rawValue = r.output,
+      ),
+    )
+  }
+
+  private def buildRule(rr: RuleResult[Input, Output], idx: Int): DiagnosticsData.Rule = {
+    val rule = table.rules(idx)
+    DiagnosticsData.Rule(
+      idx = RuleIdx(idx),
+      annotation = rule.annotation,
+      renderedConditions = {
+        given EvaluationContext[Input] = EvaluationContext.stub
+        HKDUtils
+          .collectFields(rule.matching.mapK[Const[String]]([t] => expr => expr.renderExpression))
+          .toIndexedMap(InputFieldIdx(_))
+      },
+      evaluationResults = HKDUtils.collectFields(rr.details).toIndexedMap(InputFieldIdx.apply),
+      output = rr.evaluationResult.map(result =>
+        DiagnosticsData.Rule.Output(
+          rawValue = result,
+          fieldValues = HKDUtils.collectFields(result.mapK[Const[Any]]([t] => (value: t) => value: Any)).toIndexedMap(OutputFieldIdx.apply),
+        ),
+      ),
+    )
+  }
+  
+  extension [T](v: Seq[T]) {
+    def toIndexedMap[K](createKey: Int => K): Map[K, T] = {
+      v.zipWithIndex.map((elem, idx) => (createKey(idx), elem)).toMap
+    }
+  }
+
+}
